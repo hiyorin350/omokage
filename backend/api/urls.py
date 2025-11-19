12022f07a06944a01f31f5f80dfa06d6f701dba3
hiyorin350 <mineyan777@gmail.com>
2025-11-19 22:35:02 +0900

    path修正？

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("generate/", views.generate, name="generate"),  # POST
+    path("/api/generate/", views.generate, name="generate"),  # POST
 ]
